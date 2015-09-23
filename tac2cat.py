fcaa202c012d2c7750d280d2ba940591f7b45a20
GrrrDog <agrrrdog@gmail.com>
2015-09-23 15:04:25 +0300

    Added some examples and configs tac2cat was retested

--- tac2cat.py
+++ tac2cat.py
@@ -8,10 +8,10 @@ def parse_args():
         description="A converter of a Tacacs+ packet to a format of HashCat. ")
 
     parser.add_argument(
-        '-t', '--type', type=str, help="Type of a packet. \1 - SSH only, the first packet from a cisco device to a tacacs+ server. You should set 12345678 as a username; " +
-                                       "2 - Telnet/SSH, the second authentication packet (from a tacacs+ server to a cisco device) "+
+        '-t', '--type', type=str, help="Type of a packet. 1 - SSH , the second authentication packet (from a tacacs+ server to a cisco device); " +
+                                       "2 - Telnet, the second authentication packet (from a tacacs+ server to a cisco device) "+
                                        "You should set a greeting meesage of the cisco device in -m. "+
-                                       "A default value is \"\\nUser Access Verification\\n\\nUsername: \". Set \"Password:\" for SSH",  required=True)
+                                       "A default value is \"\\nUser Access Verification\\n\\nUsername: \". Set \"Password: \" for SSH",  required=True)
     parser.add_argument(
         '-m', '--mess', type=str, help='Message for the second type', default="\nUser Access Verification\n\nUsername: ", required=False)
 
@@ -45,20 +45,20 @@ enc_data=packet[24:24+32]
 verb("Encrypted data: " + enc_data)
 
 if(p_type=="1"):
-    print("Type 1 - SSH")
-    len_mes="0%x" % len(message)
 
+    print("SSH")
+    message=message.replace('\\n', "\n")
+    mes_hex=message[0:10].encode('hex')
+    verb("Part of message in hex: "+mes_hex)
+    len_mes="0%x" % len(message)
+    #hex(len(message))[2:]
     hash_file = open("hashes.txt", "w")
-    ip_len="0%x" %(length-20)
-    verb("ip length: "+str(ip_len))
-    #headers + + static username (12345678)
-    data = "01"+ "01"+ "01" + "01"+ "08"+ "04"+ip_len+ "00" + "3132333435363738"
+    data = "05"+ "01"+ "00"+ len_mes + "00"+ "00" +mes_hex
     verb("data: "+data)
 
     md5_1=hex(int(data, 16) ^ int(enc_data, 16))[2:34]
     print("md5_1 : "+md5_1)
     hash_file.write("%s:%s%s\n" % (md5_1, vers,seq_num))
-
     print("hashes.txt was created")
     print("hashcat-cli64.exe -a 3 -m 10 --hex-charset --hex-salt hashes.txt %s?your_mask_here"%ses_id)
     hash_file.close()
@@ -66,7 +66,7 @@ if(p_type=="1"):
 
 elif(p_type=="2"):
 
-    print("Type 2")
+    print("Telnet")
     message=message.replace('\\n', "\n")
     mes_hex=message[0:10].encode('hex')
     verb("Part of message in hex: "+mes_hex)
